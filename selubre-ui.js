f1b0319a8c0541d453f678a7495eb569244222cc
Karishma Gajria <gajriakarishma123@gmail.com>
2022-02-07 22:13:43 +0530

    changes

--- selubre-ui.js
+++ selubre-ui.js
@@ -15,3 +15,12 @@ function handleSnackbarAction(){
         snackbar.classList.remove("snackbar-open")
         },2000);
 }
+
+//SLIDER - COMPONENT
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
